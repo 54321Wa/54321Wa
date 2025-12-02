4a6efaf1b4c94a0a1d51ce0a20f52357acedd0a2
Mobile User <mobile@ipad-von-walter-2.local>
2025-12-02 12:59:33 +0100

    save local changes at 2025-12-02 12:59:33

--- Pong/ main.lua
+++ Pong/ main.lua
@@ -1,13 +1,16 @@
-require "player"
+require ("player")
 
 function love.load()
+  Player:load()
 
 end
 
 function  love.update(dt)
+  Player:update(dt)
 
 end
 
 function love.draw()
+  Player:draw()
 
 end
