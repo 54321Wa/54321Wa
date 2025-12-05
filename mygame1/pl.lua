33aa87aaafb341dcaafed3027d8c107f292e39bf
Mobile User <mobile@ipad-von-walter-2.local>
2025-12-05 12:46:44 +0100

    save local changes at 2025-12-05 12:46:44

--- mygame1/pl.lua
+++ mygame1/pl.lua
@@ -7,36 +7,16 @@ function Pl:load()
  self.height = 50
  self.speed = 5
 
- button = {
-    l = {0,400,200,100,0},
-    r = {love.graphics.getWidth()-200,400,200,100,0},
-  }
-
-  --dx = 0
-  --dy = 0
  
- --freq = 60
 
-
---checkBoundaries  = true
 
 end
 
 function Pl:update(dt)
  Pl:move()
-for i,v in pairs(button) do
-    if v[5] > 0 then
-      button[i][5] = v[5] - dt
-    else 
-      button[i][5] = 0
-    end
-    
-  end
 
-  --player.x = player.x + dx * player.speed * dt
-  
-   
-  --player.y = player.y + dy * player.speed * dt
+
+ 
 
 end
 
@@ -59,40 +39,17 @@ function Pl:Boundaries()
 end
 
 function Pl:draw()
-for i,v in pairs(button) do
-  love.graphics.setColor(255,255,255)
-  love.graphics.rectangle("fill",v[1],v[2],v[3],v[4])
-  end
- love.graphics.rectangle("fill",self.x,self.y,self.width,self.height)
-end
+
 
 function Pl:mousepressed(mx,my)
-for i,v in pairs(button) do
-    
-    if mx >= v[1] and mx <= v[1] + v[3] and my >= v[2] and my <= v[2] +  v[4] then
-      
-      
-        button[i][5] = freq
-        if i == "l" then
-        
-        dx = - 20
-
-        
-
-        elseif i == "r" then
-
-        dx = 20
-        
-        end
-      end  
-    end
- 
 
 end
+ 
+
+
 
 function love.mousereleased()
-  -- Reset dx/dy when button is released to stop movement
-    dx = 0
+  
 end
 
 
