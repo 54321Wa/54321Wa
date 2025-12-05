34d86355efb5390494f5fd5c88994ac1b6ffb124
Mobile User <mobile@ipad-von-walter-2.local>
2025-12-05 18:02:14 +0100

    save local changes at 2025-12-05 18:02:14

--- Test1/main.lua
+++ Test1/main.lua
@@ -19,6 +19,7 @@ function love.load()
   button = {
     l = {0,love.graphics.getHeight()-100,200,100,0},
     r = {love.graphics.getWidth()-200,love.graphics.getHeight()-100,200,100,0},
+    j 
   }
 
   
@@ -63,6 +64,31 @@ function love.update(dt)
 
  elseif player.x + player.width > love.graphics.getWidth() then
  player.x = love.graphics.getWidth() - player.width
+
+-- Add below the right key assignment. 
+
+        -- This is in charge of player jumping.
+	if love.keyboard.isDown('space') then                     -- Whenever the player presses or holds down the Spacebar:
+
+                -- The game checks if the player is on the ground. Remember that when the player is on the ground, Y-Axis Velocity = 0.
+		if player.y_velocity == 0 then
+			player.y_velocity = player.jump_height    -- The player's Y-Axis Velocity is set to it's Jump Height.
+		end
+	end
+
+  -- Add below the jump key assignment.
+
+	-- This is in charge of the jump physics.
+	if player.y_velocity ~= 0 then                                      -- The game checks if player has "jumped" and left the ground.
+		player.y = player.y + player.y_velocity * dt                -- This makes the character ascend/jump.
+		player.y_velocity = player.y_velocity - player.gravity * dt -- This applies the gravity to the character.
+	end
+
+	-- This is in charge of collision, making sure that the character lands on the ground.
+	if player.y > player.ground then    -- The game checks if the player has jumped.
+		player.y_velocity = 0       -- The Y-Axis Velocity is set back to 0 meaning the character is on the ground again.
+		player.y = player.ground    -- The Y-Axis Velocity is set back to 0 meaning the character is on the ground again.
+	end
 
  
  end
