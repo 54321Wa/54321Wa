20c3c5e399e6a9d7ad91b4edba484140cc8bd28b
Mobile User <mobile@ipad-von-walter-2.local>
2025-12-13 19:27:06 +0100

    save local changes at 2025-12-13 19:27:06

--- MovingPlatform/main.lua
+++ MovingPlatform/main.lua
@@ -2,20 +2,22 @@ local x = 100
 local dir = 1
 
 function love.load()
-  print("console!")
+  --print("console!")
 
 end
 
 
 function love.update(dt)
  if x >= 400 or x <= 50 then
+  x = x * 60 * dt
   dir = dir * -1
  end
 end
 
 function love.draw()
-love.graphics.setColor(255,255,255)
+
+--love.graphics.setColor(255,255,255)
 love.graphics.print("Love2D Tutorial EP1",50,100)
-love.graphics.setColor(128,64,255)
+--love.graphics.setColor(128,64,255)
 love.graphics.rectangle("fill",x,200,100,50)
 end
