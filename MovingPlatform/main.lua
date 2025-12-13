fe2f848dc60564448e4064a8bfa704b4bf81da56
Mobile User <mobile@ipad-von-walter-2.local>
2025-12-13 19:42:06 +0100

    save local changes at 2025-12-13 19:42:06

--- MovingPlatform/main.lua
+++ MovingPlatform/main.lua
@@ -1,10 +1,10 @@
-require("conf(t)")
+
 
 local x = 100
 local dir = 1
 
 function love.load()
-  --print("console!")
+ love.graphics.setBackgroundColor(0,0,0)
  
 
 
@@ -21,6 +21,7 @@ function love.draw()
 
 --love.graphics.setColor(255,255,255)
 love.graphics.print("Love2D Tutorial EP1",50,100)
---love.graphics.setColor(128,64,255)
+
+love.graphics.setColor(128,64,255)
 love.graphics.rectangle("fill",x,200,100,50)
 end
