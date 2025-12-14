32fa283696e420251dc59a173148d27c2509411d
Mobile User <mobile@ipad-von-walter-2.local>
2025-12-14 13:55:10 +0100

    save local changes at 2025-12-14 13:55:10

--- Spritesheet/main.lua
+++ Spritesheet/main.lua
@@ -3,7 +3,8 @@ local hero_atlas
 function love.load()
  love.graphics.setBackgroundColor(0,0,60)
   
- hero_atlas = love.graphics.
+ hero_atlas = love.graphics.newImage("assets/gfx/hero.pn
+g")
 
 
 
@@ -20,5 +21,5 @@ function love.draw()
 love.graphics.setColor(255,0,0)
 love.graphics.print("Love2D Tutorial EP2",50,100)
 
-
+love.graphics.draw(hero_atlas,hero_sprite,120,120,)
 end
