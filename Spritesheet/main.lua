19b05fedfe0f49dbca8da6f5142374d25b40553e
Mobile User <mobile@ipad-von-walter-2.local>
2025-12-14 13:23:32 +0100

    save local changes at 2025-12-14 13:23:32

---
+++ Spritesheet/main.lua
@@ -0,0 +1,22 @@
+
+
+function love.load()
+ love.graphics.setBackgroundColor(0,0,60)
+
+
+
+end
+
+
+function love.update(dt)
+ 
+  
+end
+
+function love.draw()
+
+love.graphics.setColor(255,0,0)
+love.graphics.print("Love2D Tutorial EP2",50,100)
+
+
+end
