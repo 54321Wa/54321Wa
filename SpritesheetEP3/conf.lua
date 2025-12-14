31139d5ef21d6d93406ecc4a854da9073475f351
Mobile User <mobile@ipad-von-walter-2.local>
2025-12-14 14:28:32 +0100

    save local changes at 2025-12-14 14:28:32

---
+++ SpritesheetEP3/conf.lua
@@ -0,0 +1,14 @@
+function love.conf(t)
+  t.identy = "data/saves "
+  t.window.width = 640
+  t.window.height = 320
+  t.console = true -- console windows only
+  t.window.title="Cool Game"
+  
+  t.window.resizable = true
+  t.window.borderless = false
+
+  t.window.display = 1
+  t.window.x = 100
+
+end
