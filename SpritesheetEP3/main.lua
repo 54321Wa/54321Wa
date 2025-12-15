f795b5498f082657dd4839ad59277dda15949756
Mobile User <mobile@ipad-von-walter-2.local>
2025-12-15 12:26:33 +0100

    save local changes at 2025-12-15 12:26:33

--- SpritesheetEP3/main.lua
+++ SpritesheetEP3/main.lua
@@ -2,8 +2,10 @@ local hero_atlas
 local hero_sprite
 local angle = 0
 
+lo
+
 function love.load()
- love.graphics.setBackgroundColor(0,0,60)
+ love.graphics.setBackgroundColor(0,0,0.9)
 
  love.graphics.setDefaultFilter("nearest","nearest")
   
@@ -17,7 +19,7 @@ end
 function love.update(dt)
 if dt > 0.035 then return end
 
-angle = angle + 27.5 * dt
+--angle = angle + 27.5 * dt
  
   
 end
@@ -28,5 +30,5 @@ love.graphics.setColor(255,0,0)
 love.graphics.print("Love2D Tutorial EP3",450,100)
 
 --love.graphics.draw(hero_atlas,25,25,0,2,2)
-love.graphics.draw(hero_atlas,hero_sprite,320,160,math.rad(angle),4,4,8,8)
+love.graphics.draw(hero_atlas,hero_sprite,320,160,math.rad(angle),10,10,8,8)
 end
