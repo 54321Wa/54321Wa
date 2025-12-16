363e53c05c01a78b7588811c8ee875c74e1fbea4
Mobile User <mobile@ipad-von-walter-2.local>
2025-12-16 13:30:16 +0100

    save local changes at 2025-12-16 13:30:16

--- SpritesheetEP4/class.lua
+++ SpritesheetEP4/class.lua
@@ -6,13 +6,16 @@ function Class:new() end
 
 --create a new Class type from our base Class
 function Class:derive(type)
+  local class = {}
+  class["__call"] = Class.__call
+  
   cls.type = type
   cls.__index = cls
   cls.super = self
   setmetatable(cls,self)
   return cls
 
-function Class:__cal(...)
+function Class:__call(...)
   local inst = setmetatable({}, self)
   inst:new(...)
   return
