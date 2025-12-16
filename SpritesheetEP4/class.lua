dcfc193c32be1044929c3fa644df7e900fd0cebc
Mobile User <mobile@ipad-von-walter-2.local>
2025-12-16 12:35:16 +0100

    save local changes at 2025-12-16 12:35:16

--- SpritesheetEP4/class.lua
+++ SpritesheetEP4/class.lua
@@ -6,6 +6,12 @@ function Class:new() end
 
 --create a new Class type from our base Class
 function Class:derive(type)
+  cls.type = type
+  cls.__index = cls
+  cls.super = self
+  setmetatable(cls,self)
+  return cls
+
 
 
 function Class:get_type()
