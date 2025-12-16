7897af08a48a208b11742c64537938174bf1dd05
Mobile User <mobile@ipad-von-walter-2.local>
2025-12-16 12:30:16 +0100

    save local changes at 2025-12-16 12:30:16

--- SpritesheetEP4/class.lua
+++ SpritesheetEP4/class.lua
@@ -1,5 +1,14 @@
 local Class = {}
 Class.__index = Class
 
+--default implementation
+function Class:new() end
+
+--create a new Class type from our base Class
+function Class:derive(type)
+
+
+function Class:get_type()
+  return self.type
 
 return Class
