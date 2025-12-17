b83f1e18f68f296ace381c9381d51f012fd59db1
Mobile User <mobile@ipad-von-walter-2.local>
2025-12-17 12:45:57 +0100

    save local changes at 2025-12-17 12:45:57

---
+++ SpritesheetEP5-copy/conf.lua
@@ -0,0 +1,15 @@
+function love.conf(t)
+  t.identy = "data/saves "
+  t.window.width = 640
+  t.window.height = 320
+  t.console = true -- console windows only
+  t.window.title="Cool Game"
+  
+  t.window.resizable = true
+  t.window.borderless = false
+
+  t.window.display = 2
+  t.window.x = 100
+  t.window.y = 200
+
+end
---
+++ SpritesheetEP5-copy/main.lua
@@ -0,0 +1,52 @@
+local Anim = require("Animation")
+
+local hero_atlas
+local hero_sprite
+
+local angle = 0
+
+--animation parameters-
+local fps = 12
+local anim_timer = 1 / fps
+local frame = 1
+local num_frames = 6
+local xoffset
+-----------------------
+
+local a = Anim(16, 32, 16, 16, 6, 6, 12)
+
+function love.load()
+    love.graphics.setDefaultFilter('nearest', 'nearest')
+    hero_atlas = love.graphics.newImage("assets/gfx/hero.png")
+    hero_sprite = love.graphics.newQuad(16, 32, 16, 16, hero_atlas:getDimensions())
+
+end
+
+function love.update(dt)
+    if dt > 0.035 then return end
+
+    a:update(dt, hero_sprite)
+    -- a:update(dt, hero_sprite)
+    -- anim_timer = anim_timer - dt
+    -- if anim_timer <= 0 then
+    --     anim_timer = 1 / fps
+    --     frame = frame + 1
+    --     if frame > num_frames then frame = 1 end
+    --     xoffset = 16 * frame
+    --     hero_sprite:setViewport(xoffset, 32, 16, 16)
+    -- end
+end
+
+function love.draw()
+    love.graphics.clear(64,64,255)
+    love.graphics.draw(hero_atlas, hero_sprite, 320, 180, math.rad(angle), 10, 10, 8, 8)
+end
+
+function love.draw()
+
+--love.graphics.setColor(255,0,0)
+love.graphics.print("Love2D Tutorial EP5",450,100)
+
+--love.graphics.draw(hero_atlas,25,25,0,2,2)
+love.graphics.draw(hero_atlas,hero_sprite,320,160,math.rad(angle),10,10,8,8)
+end
