096cc4268cc069e741b8e52a08aea579f6a9bf93
Mobile User <mobile@ipad-von-walter-2.local>
2025-12-19 12:53:35 +0100

    save local changes at 2025-12-19 12:53:35

--- Test2/main.lua
+++ Test2/main.lua
@@ -164,10 +164,7 @@ function love.mousereleased()
 end
 
 function CheckCollision(x1,y1,w1,h1, x2,y2,w2,h2)
-  return x1 < x2+w2 and
-         x2 < x1+w1 and
-         y1 < y2+h2 and
-         y2 < y1+h1
+     checkCollision(pad.x, pad.y, pad.width, pad.height, ball.x, ball.y, ball.width, ball.height)
 end
 
 
