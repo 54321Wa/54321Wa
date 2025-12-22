bf16f9d567e20898f710aa44b90095e30243c2c7
Mobile User <mobile@ipad-von-walter-2.local>
2025-12-22 12:06:41 +0100

    save local changes at 2025-12-22 12:06:41

--- Test3/main.lua
+++ Test3/main.lua
@@ -3,8 +3,8 @@ function love.load()
 love.graphics.setBackgroundColor(255,0,100)
 
     
-player = {x=100,y=love.graphics.getHeight(),w=50,h=50,speed=20}
-platform = {x=300,y=100,w=100,h=100}
+player = {x=100,y=love.graphics.getHeight()-250,w=50,h=50,speed=20}
+platform = {x=300,y=400,w=100,h=100}
 player.ground = player.y
 
 
