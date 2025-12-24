ea44f82e54be39398c987c52f43cc941726840dc
Mobile User <mobile@ipad-von-walter-2.local>
2025-12-24 15:52:02 +0100

    save local changes at 2025-12-24 15:52:02

--- CollisionButton/main.lua
+++ CollisionButton/main.lua
@@ -60,7 +60,10 @@ for id = 1, #t do
   if tx > 20 and tx < 70 then
      if ty > 220 and ty < 270 then
        love.graphics.rectangle("fill",20,220,50,50,10)
-          gv = -100
+          if gc then
+             gv = -50
+             gc = false
+           end
      end
   end
 end
