c3270fb903009fd105c17d04741cfbe48df8ad2e
Mobile User <mobile@ipad-von-walter-2.local>
2025-12-24 12:42:18 +0100

    save local changes at 2025-12-24 12:42:18

--- CollisionButton/main.lua
+++ CollisionButton/main.lua
@@ -3,7 +3,7 @@ function love.load()
 love.graphics.setBackgroundColor(0,0,0)
 
     
-player = {x=350,y=love.graphics.getHeight()-250,w=50,h=50,speed=20}
+player = {x=150,y=love.graphics.getHeight()-250,w=50,h=50,speed=20}
 platform = {x=300,y=500,w=100,h=100}
 
 
