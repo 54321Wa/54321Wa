23e99a90885ce919bfe171ec25d605fe8c502fa4
Mobile User <mobile@ipad-von-walter-2.fritz.box>
2025-12-25 11:37:14 +0100

    save local changes at 2025-12-25 11:37:14

--- CollisionButton/main.lua
+++ CollisionButton/main.lua
@@ -6,10 +6,11 @@ love.graphics.setBackgroundColor(0,0,0)
 player = {x=150,y=150,w=50,h=50,speed =20}
 platform = {x=300,y=500,w=100,h=100}
 
-
-gravity = 9.81
-gravity_Vel = 0
-ground = false
+speed_no_delta = 100
+speed = 0
+ga = 9.81
+gv = 0
+gc = false
 
 end
 
@@ -72,6 +73,9 @@ end
 
 
 function love.update(dt)
+
+speed = speed_no_delta * dt
+
 ----
 if love.keyboard.isDown("right") then
   player.x = player.x + player.speed
