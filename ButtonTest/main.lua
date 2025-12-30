cd5acb04ba882805b1f9804b94bb81b623b1f7b7
Mobile User <mobile@ipad-von-walter-2.local>
2025-12-30 13:55:39 +0100

    save local changes at 2025-12-30 13:55:39

--- ButtonTest/main.lua
+++ ButtonTest/main.lua
@@ -1,11 +1,23 @@
-function love load()
+function love.load()
+devicex = 5
+devicey = 25
+KeyImg = love.graphics.newImage("arrow.png")
+sprx = 0
+spry = 0
+touches = {}
 
 end
 
-function love update(dt)
+function love.update(dt)
+touches = love.touch.getTouches()
+for i,id in pairs(touches) do
+pressure = love
+
+end
 
 
 end
 
 function love.draw()
 
+end
