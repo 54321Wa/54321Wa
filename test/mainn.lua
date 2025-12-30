c70a5d33d0710fa3d33bcd1bfdb3e71be1d79c5b
Mobile User <mobile@ipad-von-walter-2.local>
2025-12-30 13:20:53 +0100

    save local changes at 2025-12-30 13:20:53

---
+++ test/mainn.lua
@@ -0,0 +1,15 @@
+function love.load()
+   printx = 0
+   printy = 0
+end
+
+function love.draw()
+   love.graphics.print("Text", printx, printy)
+end
+
+function love.mousepressed(x, y, button, istouch)
+   if button == 1 then -- Versions prior to 0.10.0 use the MouseConstant 'l'
+      printx = x
+      printy = y
+   end
+end
