32836ffb233ec1fa6f0b460416eb40c5dbc2aa47
Mobile User <mobile@ipad-von-walter-2.local>
2026-01-01 12:30:21 +0100

    save local changes at 2026-01-01 12:30:21

--- ButtonTest-copy/main.lua
+++ ButtonTest-copy/main.lua
@@ -16,35 +16,23 @@ x,y = love.touch.getPosition(id)
 if pressure > 0 then
    spry = spry - speed
 ----
-   elseif 0+devicex <x and x<60 + devicex and 65+ devicey <y and y <190+devicey then
-         spry = spry + 1
+   elseif 0+devicex <x and x<60 + devicex and 65+ devicey <y and y <125+devicey then
+         sprx = sprx + speed
 ----
    elseif 0+devicex <x and x<60 + devicex and 130+ devicey <y and y <190+devicey then
-         sprx = sprx + 1
+         sprx = sprx + speed
 ----
     elseif 0+devicex <x and x<60 + devicex and 195+ devicey <y and y <255+devicey then
-         sprx = sprx - 1
+         sprx = sprx - speed
 
-
-end
-end
-
-end
+  end
+  end
+  end
 
 
 end
 
 function love.draw()
-love.graphics.setBackgroundColor(1,1,1)
-love.graphics.setColor(255,0,0,1)
-love.graphics.circle("fill",sprx + devicex, spry + devicey,40)
-
-love.graphics.setColor(0,0,0,0.5)
-love.graphics.draw(KeyImg, 0+devicex,0+devicey,0,0.75)
-
-love.graphics.draw(KeyImg, 60+devicex,125+devicey,math.rad(180),0.75)
-love.graphics.draw(KeyImg, 60+devicex,130+devicey,math.rad(90),0.75)
-love.graphics.draw(KeyImg, 60+devicex,255+devicey,math.rad(-90),0.75)
-
+love.graphics.draw(character,)
 
 end
