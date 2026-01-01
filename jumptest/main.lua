1b7be80d611c7d13cd73bfb5fc601c23a25f109d
Mobile User <mobile@ipad-von-walter-2.local>
2026-01-01 16:21:38 +0100

    save local changes at 2026-01-01 16:21:38

--- jumptest/main.lua
+++ jumptest/main.lua
@@ -1,7 +1,21 @@
-function love.load() player ={} player.y = 470 player.x = 0 jumping = false end
+function love.load() 
+player ={} 
+player.y = 470 
+player.x = 0 
+jumping = false 
+end
 
 function love.update() --- gravity if jumping == false then player.y = player.y + 3.1 if player.y + 10 > 470 then player.y = 470 end end end
+end
+
 
 function love.keypressed(key) if jumping == false then if key =="j" and player.y == 470 then player.y = player.y - 150 end end
 
+end
+
+function love.touchpressed( id, x, y, dx, dy, pressure )
+    -- test if the touch happened in the upper half of the screen
+    if y < love.graphics.getHeight()/2 then
+        player:jump()
+    end
 end
