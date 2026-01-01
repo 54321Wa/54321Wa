3b3f1ee09ebb25892b4bbd768110b86584d1d792
Mobile User <mobile@ipad-von-walter-2.local>
2026-01-01 12:25:21 +0100

    save local changes at 2026-01-01 12:25:21

--- ButtonTest-copy/main.lua
+++ ButtonTest-copy/main.lua
@@ -1,10 +1,9 @@
 function love.load()
-devicex = 5
-devicey = 25
-KeyImg = love.graphics.newImage("arrow.png")
-sprx = 0
-spry = 0
+
+sprx = 100
+spry = 100
 touches = {}
+speed = 10
 
 end
 
@@ -13,11 +12,11 @@ touches = love.touch.getTouches()
 for i,id in pairs(touches) do
 pressure = love.touch.getPressure(id)
 x,y = love.touch.getPosition(id)
+
 if pressure > 0 then
-   if 0+devicex <x and x<60 + devicex and 0+ devicey <y and y <60+devicey then
-         spry = spry - 1
+   spry = spry - speed
 ----
-   elseif 0+devicex <x and x<60 + devicex and 130+ devicey <y and y <190+devicey then
+   elseif 0+devicex <x and x<60 + devicex and 65+ devicey <y and y <190+devicey then
          spry = spry + 1
 ----
    elseif 0+devicex <x and x<60 + devicex and 130+ devicey <y and y <190+devicey then
