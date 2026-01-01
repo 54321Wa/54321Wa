360c5d3b30890c08700abb36adb7309215dcac71
Mobile User <mobile@ipad-von-walter-2.local>
2026-01-01 18:25:14 +0100

    save local changes at 2026-01-01 18:25:14

--- ButtonTest/main.lua
+++ ButtonTest/main.lua
@@ -14,7 +14,7 @@ for i,id in pairs(touches) do
 pressure = love.touch.getPressure(id)
 x,y = love.touch.getPosition(id)
 
-if pressure > 0 then
+if pressure >  then
    if 0+devicex <x and x<60 + devicex and 0+ devicey <y and y <60+devicey then
          spry = spry - 10
 ----
