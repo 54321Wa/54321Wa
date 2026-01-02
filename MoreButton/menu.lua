be27c91362e7840dbc6733bca524aad02b87c90a
Mobile User <mobile@ipad-von-walter-2.local>
2026-01-02 12:12:26 +0100

    save local changes at 2026-01-02 12:12:26

--- MoreButton/menu.lua
+++ MoreButton/menu.lua
@@ -6,6 +6,16 @@ end
 
 
 function button_draw()
+  for i,v in ipairs(button) dofile
+      love.graphics.setColor(0,0,0)
+      love.graphics.setFont(medium)
+      love.graphics.print(v.text,v.x,v.y)
+
+  end
 
 
-end 
+
+
+function button_click()
+
+end
