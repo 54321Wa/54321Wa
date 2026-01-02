d8ca80897540619c2dfcd135a4eb0e0a7733ba16
Mobile User <mobile@ipad-von-walter-2.local>
2026-01-02 12:17:43 +0100

    save local changes at 2026-01-02 12:17:43

---
+++ MoreButton/main.lua
@@ -0,0 +1,21 @@
+require "menu"
+
+
+function love.load()
+medium = love.graphics.newFont(45)
+
+gamestat = "menu"
+
+love.graphics.setBackgroundColor(
+
+end
+
+
+function love.update(dt)
+
+end
+
+
+function love.draw()
+
+end
--- MoreButton/menu.lua
+++ MoreButton/menu.lua
@@ -6,13 +6,13 @@ end
 
 
 function button_draw()
-  for i,v in ipairs(button) dofile
+  for i,v in ipairs(button) do
       love.graphics.setColor(0,0,0)
       love.graphics.setFont(medium)
       love.graphics.print(v.text,v.x,v.y)
 
   end
-
+end
 
 
 
