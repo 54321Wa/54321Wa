592a4fb4f98dce3663dba515313992414a7925e2
Mobile User <mobile@ipad-von-walter-2.local>
2026-01-02 12:43:59 +0100

    save local changes at 2026-01-02 12:43:59

--- MoreButton/player.lua
+++ MoreButton/player.lua
@@ -5,7 +5,7 @@ player.speed = 50
 player.health = 20
 player.damage = 2
 
-player.pic = love.graphics.newImage(red.png)
+player.pic = love.graphics.newImage("red.png")
 
 function player_draw()
   love.graphics.set.color(255,255,255)
