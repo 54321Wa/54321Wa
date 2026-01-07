a47a1af78e32e12a6beeff98cba4e69a60a2edf4
Mobile User <mobile@ipad-von-walter-2.local>
2026-01-07 13:04:08 +0100

    save local changes at 2026-01-07 13:04:08

--- MyGame/game.lua
+++ MyGame/game.lua
@@ -1,4 +1,9 @@
-require "player"
+player = {}
+player.x = 100
+player.y = 100
+player.speed = 50
+player.health = 20
+player.damage = 2
 
 function game_load()
 
@@ -14,6 +19,24 @@ end
 
 
 function game_draw()
+ player.pic = love.graphics.newImage("red.png")
+  
+  love.graphics.draw(player.pic, player.x,player.y)
 
 
+end
+
+function game_move()
+  if love.keyboard.isDown("up") then
+     player.y = player.y - player.speed
+  end
+  if love.keyboard.isDown("down") then
+     player.y = player.y + player.speed
+  end
+  if love.keyboard.isDown("left") then
+     player.x = player.x - player.speed
+  end
+  if love.keyboard.isDown("right") then
+     player.x = player.x + player.speed
+  end
 end
